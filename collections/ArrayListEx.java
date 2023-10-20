bc61bcad5872e1d7f4a38e0f2a22c1abbdf78b44
PEDRO HENRIQUE GUEDES DE ALMEIDA <pedrohenrique.guedes@a.ucb.br>
2023-10-20 09:41:21 -0300

    ArrayList

---
+++ collections/ArrayListEx.java
@@ -0,0 +1,26 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class ArrayListEx {
+    public static void main(String[] args) {
+        ArrayList<String> listaDeNomes = new ArrayList<>();
+        listaDeNomes.add("Alice");
+        listaDeNomes.add("Bob");
+        listaDeNomes.add("Carol");
+
+        for (String nome : listaDeNomes) {
+            System.out.println(nome);
+        }
+
+        if (listaDeNomes.contains("Carol")) {
+            System.out.println("Carol esta na lista");
+        }
+        
+        listaDeNomes.remove("Carol");
+
+        if (!listaDeNomes.contains("Carol")) {
+            System.out.println("Carol nao esta na lista");
+        }
+    }
+}
